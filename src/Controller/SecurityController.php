d2e56e2b783baf1daf582c72f25e5788079cbe18
Arnaud BODEL <contact@osw3.net>
2022-06-24 14:58:20 +0200

    -

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route( "/", name="security:api" )
+ */
+class SecurityController extends AbstractController
+{
+}
