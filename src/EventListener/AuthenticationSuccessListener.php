9593ad0a7b699bc0f6c595f8447813a1f685e843
Arnaud BODEL <contact@osw3.net>
2022-06-24 15:18:33 +0200

    -

--- src/EventListener/AuthenticationSuccessListener.php
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -5,21 +5,18 @@ use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 
 class AuthenticationSuccessListener
 {
-    /**
-     * @param AuthenticationSuccessEvent $event
-     */
     public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
     {
-
-        $data = $event->getData();
         $user = $event->getUser();
 
-        // Add custom data
-        $data['id'] = $user->getId();
-        $data['firstname'] = $user->getFirstname();
-        $data['lastname'] = $user->getLastname();
-        $data['email'] = $user->getEmail();
-    
-        $event->setData($data);
+        $event->setData([
+            'code' => $event->getResponse()->getStatusCode(),
+            'payload' => $event->getData(),
+
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'email' => $user->getEmail(),
+        ]);
     }
 }
